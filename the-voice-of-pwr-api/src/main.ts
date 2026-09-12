3574d04e1ba0db646f684646120981186e29f2a7
Nadia Gill <nadia.gill2006@gmail.com>
2026-09-12 12:42:29 +0200

    feat: swagger (#5)

--- the-voice-of-pwr-api/src/main.ts
+++ the-voice-of-pwr-api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('Głos PWR API')
+    .setDescription(
+      'The Głos PWR API allows to fetch posts, lecturers and everything related to the site.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  (await app.listen(process.env.PORT ?? 3000), '127.0.0.1');
 }
 bootstrap();
